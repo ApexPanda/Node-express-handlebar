02a4495179ec52ba69f531eafa2aab43a625c69c
Kayla Tryon <dawnka22@gmail.com>
2019-05-15 10:37:22 -0600

    mysql schema and seeds created

---
+++ database/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers(burger_name, devoured) VALUES ("BaconBurger", true);
+INSERT INTO burgers(burger_name, devoured) VALUES ("CheeseBurger", false);
+INSERT INTO burgers(burger_name, devoured) VALUES ("VeggieBurger", true);
+INSERT INTO burgers(burger_name, devoured) VALUES ("PlainBurger", false);
