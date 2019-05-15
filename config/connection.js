95094a20899192e5e1f8eb14d8611b9a567f5c05
Kayla Tryon <dawnka22@gmail.com>
2019-05-15 09:10:43 -0600

    connection setup built

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+var mysql = require("mysql");
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    })
+};
+
+//Make the connection 
+
+connection.connect(function(err){
+    if(err){
+        console.error(`Connected as id ${connection.threadId}`);
+    }
+});
+
+//Exports connection to ORM file
+module.exports = connection;
